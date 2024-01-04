a65bc55b5a9992442ef4ec83088eda87d1ac01df
Leo-BH <leoaugusto863@gmail.com>
2024-01-04 16:45:15 -0300

    Melhoria na impressão dos dados

--- desenv_3.js
+++ desenv_3.js
@@ -3,10 +3,10 @@ numerosDaSorte = [37, 14, 26, 5, 94, 87]
 for(let i=0; i< numerosDaSorte.length; i++){
     console.log(numerosDaSorte[i]);
     if (numerosDaSorte[i] %2 == 0 && numerosDaSorte[i] < 50) {
-      console.log(" X é par e menor que 50");
+      console.log(numerosDaSorte[i] + " é par e menor que 50");
     } else if(numerosDaSorte[i] < 50){
-      console.log("X é menor que 50");
+      console.log(numerosDaSorte[i] + " é menor que 50");
     } else {
-      console.log("X é maior que 50");
+      console.log(numerosDaSorte[i] + " é maior que 50");
     }
 }
